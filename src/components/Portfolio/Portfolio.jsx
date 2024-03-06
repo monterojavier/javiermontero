bfcc5a711283bba05fab5376872fc5d83293e7a9
Javier Montero <monterodotjavier@icloud.com>
2024-03-06 18:26:25 -0500

    created portfolio page

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Portfolio.css'
+
+function Portfolio() {
+  return (
+    <article className='portfolio' data-page='portfolio'>
+      <header>
+        <h2 className='h2 article-title'>Portfolio</h2>
+      </header>
+    </article>
+  )
+}
+
+export default Portfolio
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
-import Navbar from "./Navbar/Navbar.jsx";
-import Sidebar from "./Sidebar/Sidebar.jsx";
-import Resume from "./Resume/Resume.jsx";
-import About from "./About/About.jsx";
+import Navbar from './Navbar/Navbar.jsx'
+import Sidebar from './Sidebar/Sidebar.jsx'
+import About from './About/About.jsx'
+import Resume from './Resume/Resume.jsx'
+import Portfolio from './Portfolio/Portfolio.jsx'
 
-export { Navbar, Sidebar, Resume, About };
+export { Navbar, Sidebar, About, Resume, Portfolio }

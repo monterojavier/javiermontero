72320a3fa4d6f6b54662da2459c6173a9ec69a62
Javier Montero <monterodotjavier@icloud.com>
2024-03-06 21:14:08 -0500

    feat: added a portfolio section

---
+++ src/components/Portfolio/projectList.js
@@ -0,0 +1,36 @@
+// Image imports
+import shoppingListScreenshot from '../../images/shopping-list-screenshot.png'
+
+// Links to Deployed Site or Github Project
+const shoppingListLink = 'https://monterojavier.github.io/simple-shopping-list/'
+
+export default [
+  {
+    id: 1,
+    link: shoppingListLink,
+    alt: 'Shopping List',
+    src: shoppingListScreenshot,
+    title: 'Shopping List',
+    category: 'Web Development',
+    dataCategory: 'web development'
+  }
+]
+
+/* dataCategory & category ex:
+    - web development
+    - mobile
+
+    https://github.com/monterojavier/simple-shopping-list
+
+    Sample element:
+
+    {
+      id: 1,
+      link: 'https://github.com/',
+      alt: 'Alt name',
+      src: projectPlaceholderImage,
+      title: 'MAPPP WORKSSS',
+      category: 'Web Development',
+      dataCategory: 'web development'
+    }
+*/
